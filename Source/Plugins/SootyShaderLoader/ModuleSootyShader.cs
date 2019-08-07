1eb65bfe52efe23cad55768e479aee74e045fc55
blowfish <joeydwong@gmail.com>
2019-08-07 00:06:38 -0700

    Add DMagic's shaders and code from Tundra
    done with permission
    
    With a few small tweaks

---
+++ Source/Plugins/SootyShaderLoader/ModuleSootyShader.cs
@@ -0,0 +1,226 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using UnityEngine;
+
+namespace SootyShaderLoader
+{
+    public class ModuleSootyShader : PartModule
+    {
+        [KSPField]
+        public string ShaderName;
+        [KSPField]
+        public string ObjectNames;
+        [KSPField]
+        public string TextureID;
+        [KSPField]
+        public string TextureName;
+        [KSPField]
+        public bool ShowTransitionEvent = true;
+        [KSPField]
+        public bool ShowTransitionAction = true;
+        [KSPField]
+        public float SootySpeed = 2;
+        [KSPField]
+        public bool OneShot = false;
+        [KSPField]
+        public bool EVAClean = false;
+        [KSPField(isPersistant = true)]
+        public bool SootyState = false;
+
+        private float materialState = 0;
+
+        public bool loaded;
+
+        private Coroutine TransitionRoutine;
+
+        private List<Transform> ModelObjects = new List<Transform>();
+
+        public override void OnStart(StartState state)
+        {
+            base.OnStart(state);
+
+            Events["ToggleSooty"].active = ShowTransitionEvent;
+            Actions["ToggleSootyAction"].active = ShowTransitionAction;
+            Events["CleanSoot"].active = EVAClean;
+
+            if (!loaded)
+                return;
+            
+            if (string.IsNullOrEmpty(ObjectNames))
+                return;
+
+            ModelObjects = new List<Transform>();
+
+            var names = ObjectNames.Split(';');
+
+            for (int i = names.Length - 1; i >= 0; i--)
+            {
+                string name = names[i];
+
+                ModelObjects.AddRange(part.FindModelTransforms(name));
+            }
+
+            if (SootyState)
+            {
+                materialState = 1;
+
+                SetMaterialState();
+            }
+        }
+
+        public override void OnLoad(ConfigNode node)
+        {
+            base.OnLoad(node);
+
+            if (loaded)
+                return;
+
+            //print("[ShangshengOrbital] - Load shader from OnLoad");
+            LoadShader();
+        }
+
+        private void LoadShader()
+        {
+            if (string.IsNullOrEmpty(ObjectNames))
+                return;
+
+            if (string.IsNullOrEmpty(ShaderName))
+                return;
+
+            Shader shade = GameDatabase.Instance.GetShader(ShaderName);
+
+            if (shade == null)
+            {
+                print("[ShangshengOrbital] - Error loading shader from GameDatabase: " + ShaderName);
+                shade = SootyShaderLoader.SootyShader;
+            }
+
+            if (shade == null)
+                return;
+
+            Texture tex = GameDatabase.Instance.GetTexture(TextureName, false);
+
+            if (tex == null)
+                return;
+
+            var names = ObjectNames.Split(';');
+
+            for (int i = names.Length - 1; i >= 0; i--)
+            {
+                string name = names[i];
+
+                ModelObjects.AddRange(part.FindModelTransforms(name));
+            }
+
+            for (int i = ModelObjects.Count - 1; i >= 0; i--)
+            {
+                Renderer render = ModelObjects[i].gameObject.GetComponent<Renderer>();
+
+                if (render == null)
+                    continue;
+
+                render.material.shader = shade;
+
+                render.material.SetTexture(TextureID, tex);
+            }
+
+            loaded = true;
+
+            print("[ShangshengOrbital] - Processed shader switch...");
+        }
+
+        [SuppressMessage("Style", "IDE0060", Justification = "KSPAction requires action param even if unused")]
+        [KSPAction("Toggle Soot")]
+        public void ToggleSootyAction(KSPActionParam action)
+        {
+            ToggleSooty();
+        }
+
+        [KSPEvent(guiActive = true, guiActiveEditor = true, guiName = "Toggle Soot", active = true)]
+        public void ToggleSooty()
+        {
+            if (SootyState && OneShot && HighLogic.LoadedSceneIsFlight)
+                return;
+
+            SootyState = !SootyState;
+
+            if (TransitionRoutine != null)
+                StopCoroutine(TransitionRoutine);
+
+            //print("[ShangshengOrbital] - Toggle soot state");
+
+            TransitionRoutine = StartCoroutine(AnimateMaterial());
+        }
+
+        [KSPEvent(guiActive = false, guiActiveEditor = false, guiActiveUnfocused = true, guiName = "Clean Soot", active = false)]
+        public void CleanSoot()
+        {
+            if (!SootyState)
+                return;
+
+            if (FlightGlobals.ActiveVessel != null && FlightGlobals.ActiveVessel.isEVA)
+            {
+                SootyState = false;
+
+                if (TransitionRoutine != null)
+                    StopCoroutine(TransitionRoutine);
+
+                //print("[ShangshengOrbital] - Clean soot state");
+
+                TransitionRoutine = StartCoroutine(AnimateMaterial());
+            }
+        }
+
+        private IEnumerator AnimateMaterial()
+        {
+            if (SootyState)
+            {
+                while (materialState < 1)
+                {
+                    materialState = Mathf.Lerp(materialState, 1, TimeWarp.deltaTime * SootySpeed);
+                    //print("[ShangshengOrbital] - Setting soot to: " + materialState.ToString());
+
+                    if (materialState > 0.99f)
+                        materialState = 1;
+
+                    SetMaterialState();
+
+                    yield return null;
+                }                    
+            }
+            else
+            {
+                while (materialState > 0)
+                {
+                    materialState = Mathf.Lerp(materialState, 0, TimeWarp.deltaTime * SootySpeed);
+                    //print("[ShangshengOrbital] - Setting soot to: " + materialState.ToString());
+
+                    if (materialState < 0.001f)
+                        materialState = 0;
+
+                    SetMaterialState();
+
+                    yield return null;
+                }
+            }
+
+            TransitionRoutine = null;
+        }
+
+        private void SetMaterialState()
+        {
+            for (int i = ModelObjects.Count - 1; i >= 0; i--)
+            {
+                Renderer render = ModelObjects[i].gameObject.GetComponent<Renderer>();
+
+                if (render == null)
+                    continue;
+
+                render.material.SetFloat("_State", materialState);
+            }
+        }
+    }
+
+}
---
+++ Source/Plugins/SootyShaderLoader/SootyShaderLoader.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using UnityEngine;
+
+namespace SootyShaderLoader
+{
+    [KSPAddon(KSPAddon.Startup.Instantly, true)]
+    public class SootyShaderLoader : MonoBehaviour
+    {
+        private const string shadersAssetName = "ShangshengOrbitalShader";
+        private const string winShaderName = "-windows.ksp";
+        private const string linuxShaderName = "-linux.ksp";
+        private const string macShaderName = "-macosx.ksp";
+
+        private static bool loaded;
+        
+        public static Shader SootyShader { get; private set; }
+
+        [SuppressMessage("Code Quality", "IDE0051", Justification = "Called by Unity")]
+        private void Start()
+        {
+            if (loaded)
+                return;
+
+            LoadBundle();
+        }
+
+        //This code is borrowed from Textures Unlimited: https://github.com/shadowmage45/TexturesUnlimited/blob/master/Plugin/SSTUTools/KSPShaderTools/Addon/TexturesUnlimitedLoader.cs#L198-L254
+        private void LoadBundle()
+        {
+            string shaderPath;
+
+            if (Application.platform == RuntimePlatform.WindowsPlayer && SystemInfo.graphicsDeviceVersion.StartsWith("OpenGL"))
+                shaderPath = shadersAssetName + linuxShaderName;
+            else if (Application.platform == RuntimePlatform.WindowsPlayer)
+                shaderPath = shadersAssetName + winShaderName;
+            else if (Application.platform == RuntimePlatform.LinuxPlayer)
+                shaderPath = shadersAssetName + linuxShaderName;
+            else
+                shaderPath = shadersAssetName + macShaderName;
+
+            shaderPath = KSPUtil.ApplicationRootPath + "GameData/ShangshengOrbital/Resources/" + shaderPath;
+
+            // KSP-PartTools built AssetBunldes are in the Web format, 
+            // and must be loaded using a WWW reference; you cannot use the
+            // AssetBundle.CreateFromFile/LoadFromFile methods unless you 
+            // manually compiled your bundles for stand-alone use
+
+            AssetBundle bundle;
+
+            using (WWW www = CreateWWW(shaderPath))
+            {
+                if (!string.IsNullOrEmpty(www.error))
+                {
+                    print("[ShangshengOrbital] - Error while loading shader AssetBundle: " + www.error);
+                    return;
+                }
+                else if (www.assetBundle == null)
+                {
+                    print("[ShangshengOrbital] - Could not load AssetBundle from WWW - " + www);
+                    return;
+                }
+
+                bundle = www.assetBundle;
+            }
+
+            string[] assetNames = bundle.GetAllAssetNames();
+            int len = assetNames.Length;
+            Shader shader;
+            for (int i = 0; i < len; i++)
+            {
+                if (assetNames[i].EndsWith(".shader"))
+                {
+                    shader = bundle.LoadAsset<Shader>(assetNames[i]);
+                    print("[ShangshengOrbital] - Loaded Shader: " + shader.name + " :: " + assetNames[i] + " from path: " + shaderPath);
+                    if (shader == null || string.IsNullOrEmpty(shader.name))
+                    {
+                        print("[ShangshengOrbital] - ERROR: Shader did not load properly for asset name: " + assetNames[i]);
+                    }
+                    GameDatabase.Instance.databaseShaders.AddUnique(shader);
+                    SootyShader = shader;
+                }
+            }
+            //this unloads the compressed assets inside the bundle, but leaves any instantiated shaders in-place
+            bundle.Unload(false);
+
+            loaded = true;
+        }
+
+        private static WWW CreateWWW(string bundlePath)
+        {
+            try
+            {
+                string name = Application.platform == RuntimePlatform.WindowsPlayer ? "file:///" + bundlePath : "file://" + bundlePath;
+                return new WWW(Uri.EscapeUriString(name));
+            }
+            catch (Exception e)
+            {
+                print("[ShangshengOrbital] - Error while creating AssetBundle request: " + e);
+                return null;
+            }
+        }
+    }
+}
